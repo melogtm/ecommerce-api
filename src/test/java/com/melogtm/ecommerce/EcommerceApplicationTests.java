e51763d2aa112389ef24fcd7ce9dd4cf68fef330
melogtm <gabrielcursos@proton.me>
2025-01-14 19:52:29 -0300

    first commit

---
+++ src/test/java/com/melogtm/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melogtm.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
